dae6685e3297f6fc6dad3acadfad52716689c362
Diana Leyva Solis <168212795+DianaLeyva@users.noreply.github.com>
2025-02-07 16:52:04 +0000

    new file:   funcion1.dart

---
+++ funcion1.dart
@@ -0,0 +1,17 @@
+//funcion que no devuelve valor
+//funcion saludar
+void saludar(){
+  print("Hola Mundo");
+}
+void escribeNombre(){
+  print("Diana Leyva");
+}
+void main(){
+  //Diana Leyva 22308051281071
+  //llamar a la funcion saludar
+  print("Diana Leyva: 22308051281071");
+  print("llamando a la funcion saludar");
+  saludar();
+  print("llamando a la funcion escribeNombre");
+  escribeNombre();
+}
