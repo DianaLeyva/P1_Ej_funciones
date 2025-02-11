c5a1331df4166b3ccc839dec7871e62058133df7
Diana Leyva Solis <168212795+DianaLeyva@users.noreply.github.com>
2025-02-11 20:32:14 +0000

    corregimos captura

--- problema7.dart
+++ problema7.dart
@@ -1,26 +1,37 @@
-void main() {
-  // Lista de cadenas de ejemplo
-  List<String> listaCadenas = [
-    "Hola mundo",
-    "hello world",
-    "bonjour le monde",
-    "ciao mondo",
-  ];
+import 'dart:io';
 
-  // Variable para almacenar el total de palabras
+void main() {
+
+  List<String> listaCadenas = [];
+
+  print("Ingrese las cadenas (escriba 'fin' para terminar):");
+
+  
+  while (true) {
+    String entrada = stdin.readLineSync()!.trim();
+    if (entrada.toLowerCase() == 'fin') {
+      break;
+    }
+    listaCadenas.add(entrada); 
+  }
+
+
   int totalPalabras = 0;
 
-  // Recorrer cada cadena en la lista
+
   for (String cadena in listaCadenas) {
-    // Dividir la cadena en palabras usando el espacio como separador
+    
     List<String> palabras = cadena.split(' ');
-    // Sumar el número de palabras al total
+    
     totalPalabras += palabras.length;
   }
+
+
+  print("\nCadenas ingresadas:");
   for (int i = 0; i < listaCadenas.length; i++) {
-    print("${i+1} - ${listaCadenas[i]}");
+    print("${i + 1} - ${listaCadenas[i]}");
   }
 
-  // Mostrar el resultado
-  print("Total de palabras en todas las cadenas: $totalPalabras");
+
+  print("\nTotal de palabras en todas las cadenas: $totalPalabras");
 }
--- recursos/problema7.md
+++ recursos/problema7.md
@@ -1,3 +1,3 @@
 Problema 7 
 Implementa un programa que tome una lista de cadenas (string) y cuente 8 cuántas palabras hay en total en todas las cadenas.
-![alt text](image-5.png)
+![alt text](image-6.png)
